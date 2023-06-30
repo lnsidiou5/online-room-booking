b820b74b1651689dda42e5a7cf350af9c43421a7
lnsidiou5 <21jason.li@gmail.com>
2023-06-30 00:27:00 -0400

    Updated

--- backend/src/test/java/com/myproject/roombooking/RoombookingApplicationTests.java
+++ backend/src/test/java/com/myproject/roombooking/RoombookingApplicationTests.java
@@ -1,10 +1,11 @@
-package com.laioffer.staybooking;
+package com.myproject.roombooking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-class StaybookingApplicationTests {
+class RoombookingApplicationTests {
 
     @Test
     void contextLoads() {
